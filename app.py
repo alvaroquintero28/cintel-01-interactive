ea3c9bfafdde92827adbe6975034a6e8444abcf1
alvaroquintero28 <162182742+alvaroquintero28@users.noreply.github.com>
2024-03-07 18:17:25 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,40 @@
 "Alvaro Quintero"
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
 from shiny.express import ui, input, render
 
+# Add page options for the overall app.
+ui.page_opts(title="To Be or Not To Be", fillable=True)
+
+# create a sidebar with slider input
 with ui.sidebar():
-    ui.input_slider:("selected_number_of_bins")
-    ui.input_slider("n", "Number of Bins", 0, 100, 20 )
+    # Add a slider for specifying the number of bins in the histogram.
+    # The ui.input_slider function is called with 5 arguments:
+    # 1. A string id ("selected_number_of_bins") that uniquely identifies this input.
+    # 2. A string label (e.g., "Number of Bins") to be displayed alongside the slider.
+    # 3. An integer representing the minimum number of bins
+    # 4. An integer representing the maximum number of bins
+    # 5. An integer representing the initial value of the slider
+    ui.input_slider(
+        "selected_number_of_bins",  # Unique ID for the input value
+        "Number of Bins",  # Label displayed alongside the slider
+        0,  # Minimum number of bins
+        100,  # Maximum number of bins
+        20,  # Initial value of the slider
+    )
 
-
-@render.plot(alt="A histogram showing random data distribution")
+# Define the reactive output chart functions
+@render.plot(alt="A histogram")
 def histogram():
     np.random.seed(3)
     x = 100 + 15 * np.random.randn(437)
-    plt.hist(x, input.n(), density=True, color='red') 
+    # Create a histogram plot using the selected number of bins and red color
+    plt.hist(x, input.selected_number_of_bins(), density=True, color='red')
+
+@render.plot(alt="A histogram")
+def scatterplot():
+    np.random.seed(34)
+    x = 100 + 15 * np.random.normal(size=10)
+    y = 1 + 1 * np.random.normal(size=10)
+    sns.scatterplot(x=x, y=y, color='black')
